6e63b8c2bd9f7dd6f03e0adc651b7322e25166e9
Zack <zackhoang2004@gmail.com>
2025-07-27 15:39:07 -0700

    fix: refactor checking for existing folder

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,16 +2,38 @@ const { isLoggedIn } = require("../auth/passportConfig");
 const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
+const checkRootFolder = async (req, res, next) => {
+    const isRootExist = await prisma.folder.findFirst({
+        where: {
+            author: req.user.username, 
+            name: "root"
+        }
+    });
+    if (isRootExist === null) {
+        await prisma.folder.create({
+            data: {
+                name: "root",
+                author: req.user.username,
+            }
+        });
+    }
+    return next();
+}
+
 exports.displayHome = [
     isLoggedIn,
+    checkRootFolder,
     async(req, res) => {
-        const isFolderExist = await prisma.folder.findUnique({
-            where: {
-                id: req.params.parentID
+        if (req.params.parentID !== "root") {
+            const isFolderExist = await prisma.folder.findUnique({
+                where: {
+                    id: req.params.parentID,
+                    author: req.user.username
+                }
+            }); 
+            if (isFolderExist === null) {
+                return res.redirect("/home/root");
             }
-        }).length > 0 || req.params.parentID === "root"; 
-        if (isFolderExist === false) {
-            return res.redirect("/home/root");
         }
         const folders = await prisma.folder.findMany({
             where: {
